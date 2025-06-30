47701a90987dcdefd5b60d0d3f05ba3e906096c2
Arun Kishore <50844460+rpakishore@users.noreply.github.com>
2025-06-29 21:16:21 -0700

    -

--- src/jiosaavn/API/SaavnMe_Parser.py
+++ src/jiosaavn/API/SaavnMe_Parser.py
@@ -1,116 +1,135 @@
-from ak_requests import RequestsSession
-
 import html
 import json
 import re
 from typing import Literal
 
+from ak_requests import RequestsSession
+
 from jiosaavn.file_parser import Song
 from jiosaavn.utils import log
 
+
 class SaavnMe:
-    BASEURL: str = 'https://saavn.dev/'
+    BASEURL: str = "https://saavn.dev/"
     SESSION = RequestsSession()
-    log.info('SaavnMe Instance Initialized')
-    
+    log.info("SaavnMe Instance Initialized")
+
     def __init__(self) -> None:
-        self.SESSION.MIN_REQUEST_GAP = 1.5  #To prevent Ratelimit in free API
-    
+        self.SESSION.MIN_REQUEST_GAP = 1.5  # To prevent Ratelimit in free API
+
     def __str__(self) -> str:
-        return 'Instance of SaavnMe class for saavn.me parser'
-    
+        return "Instance of SaavnMe class for saavn.me parser"
+
     def __repr__(self) -> str:
-        return 'SaavnMe()'
-    
-    def url(self, type: Literal['playlist', 'song']) -> str:
+        return "SaavnMe()"
+
+    def url(self, type: Literal["playlist", "song"]) -> str:
         match type:
-            case 'playlist':
-                return f'{self.BASEURL}api/playlists'
-            case 'song':
-                return f'{self.BASEURL}api/songs'
-            
-    def playlist(self, id: int|str|None = None, link: str|None = None) -> list[Song]:
+            case "playlist":
+                return f"{self.BASEURL}api/playlists"
+            case "song":
+                return f"{self.BASEURL}api/songs"
+            case "search_song":
+                return f"{self.BASEURL}api/search/songs"
+
+    def search_song(self, query: str) -> list[Song]:
+        """Returns a list of Song dataclass from the search query"""
+        req_url: str = f"{self.url(type='search_song')}?query={query}&limit=100"
+        _res = self.SESSION.get(req_url)
+        data: dict = json.loads(_res.content.decode())["data"]
+        return _parse_playlist_results(data["results"])
+
+    def playlist(
+        self, id: int | str | None = None, link: str | None = None
+    ) -> list[Song]:
         """Provides a list of Song dataclass from the playlist id"""
         if id is None and link is None:
-            raise Exception('Playlist ID or Link is required')
+            raise Exception("Playlist ID or Link is required")
         if id is not None and link is not None:
-            raise Exception('Provide only one of playlist or link')
+            raise Exception("Provide only one of playlist or link")
         if id is not None:
-            log.info(f'Extracting Playlist Info with ID: {id}')
+            log.info(f"Extracting Playlist Info with ID: {id}")
             url: str = f"{self.url(type='playlist')}?id={id}&limit=1000"
         else:
-            if link.startswith('www'):
-                link = f'https://{link}'
-            log.info(f'Extracting Playlist Info with Link: {link}')
+            if link.startswith("www"):
+                link = f"https://{link}"
+            log.info(f"Extracting Playlist Info with Link: {link}")
             url: str = f"{self.url(type='playlist')}?link={link}&limit=1000"
         _res = self.SESSION.get(url)
-        data: dict = json.loads(_res.content.decode())['data']
-        
-        log.debug(f'Playlist: {data.get("name")}\nSongCount:{data.get("songCount")}\nFollowers:{data.get("followerCount")}\nURL:{data.get("url")}')
-                
-        return _parse_playlist_results(
-            song_list=data['songs']
-            )
-    
+        data: dict = json.loads(_res.content.decode())["data"]
+
+        log.debug(
+            f"Playlist: {data.get('name')}\nSongCount:{data.get('songCount')}\nFollowers:{data.get('followerCount')}\nURL:{data.get('url')}"
+        )
+
+        return _parse_playlist_results(song_list=data["songs"])
+
     def song(self, url: str) -> Song:
         """Returns Song dataclass from provided jiosaavn url"""
-        req_url: str = f'{self.url(type="song")}?link={url}'
-        log.info(f'Extracting Song from URL: {url}')
+        req_url: str = f"{self.url(type='song')}?link={url}"
+        log.info(f"Extracting Song from URL: {url}")
         _res = self.SESSION.get(req_url)
-        data: dict = json.loads(_res.content.decode())['data'][0]
+        data: dict = json.loads(_res.content.decode())["data"][0]
         return _parse_song_dict(song_dict=data)
-    
+
+
 def _parse_song_dict(song_dict: dict) -> Song:
     return Song(
-                song_id=song_dict['id'],
-                name=song_name(song_dict=song_dict),
-                album=album_name(song_dict=song_dict),
-                media_url=media_url(song_dict=song_dict),
-                primary_artists=artists(song_dict=song_dict, type='primary'),
-                artists=artists(song_dict=song_dict, type='all'),
-                year=int(song_dict['year']),
-                image_url=image_url(song_dict=song_dict)
-            )
+        song_id=song_dict["id"],
+        name=song_name(song_dict=song_dict),
+        album=album_name(song_dict=song_dict),
+        media_url=media_url(song_dict=song_dict),
+        primary_artists=artists(song_dict=song_dict, type="primary"),
+        artists=artists(song_dict=song_dict, type="all"),
+        year=int(song_dict["year"]),
+        image_url=image_url(song_dict=song_dict),
+    )
+
 
 def _parse_playlist_results(song_list: list[dict]) -> list[Song]:
     return [_parse_song_dict(song_dict) for song_dict in song_list]
 
+
 def media_url(song_dict: dict) -> str:
-    media_url: str = ''
-    download_urls: list[dict] = song_dict['downloadUrl']
+    media_url: str = ""
+    download_urls: list[dict] = song_dict["downloadUrl"]
     _currentkbps: int = 0
-    
+
     for download_url in download_urls:
-        __curr = int(download_url['quality'].replace('kbps',''))
+        __curr = int(download_url["quality"].replace("kbps", ""))
         if __curr > _currentkbps:
-            media_url = download_url['url']
+            media_url = download_url["url"]
             _currentkbps = __curr
-    
+
     return media_url
 
-def artists(song_dict: dict, type: Literal['primary', 'all']) -> list[str]:
-    return [artist['name'] for artist in song_dict['artists'].get(type)]
+
+def artists(song_dict: dict, type: Literal["primary", "all"]) -> list[str]:
+    return [artist["name"] for artist in song_dict["artists"].get(type)]
+
 
 def image_url(song_dict: dict) -> str:
-    urls: list[dict] = song_dict['image']
+    urls: list[dict] = song_dict["image"]
     _currentkbps: int = 0
-    image_url: str = ''
-    
+    image_url: str = ""
+
     for url in urls:
-        __curr = int(url['quality'].split('x')[0])
+        __curr = int(url["quality"].split("x")[0])
         if __curr > _currentkbps:
-            image_url = url['url']
+            image_url = url["url"]
             _currentkbps = __curr
     return image_url
 
+
 def song_name(song_dict: dict) -> str:
-    name: str = html.unescape(song_dict['name'])
-    if match:= re.match(r'^(.*) \(From "(.*)"\)$', name.strip()):
+    name: str = html.unescape(song_dict["name"])
+    if match := re.match(r'^(.*) \(From "(.*)"\)$', name.strip()):
         return match.group(1)
     return name.strip()
 
+
 def album_name(song_dict: dict) -> str:
-    name: str = html.unescape(song_dict['album']['name'])
-    if match:= re.match(r'^(.*) \(From "(.*)"\)$', name.strip()):
+    name: str = html.unescape(song_dict["album"]["name"])
+    if match := re.match(r'^(.*) \(From "(.*)"\)$', name.strip()):
         return match.group(2)
     return name.strip()
--- src/jiosaavn/main.py
+++ src/jiosaavn/main.py
@@ -1,99 +1,129 @@
-from jiosaavn.api_parser import SaavnAPI
+from pathlib import Path
+
+import tomllib
+
 from jiosaavn.API import SaavnMe
 from jiosaavn.file_parser import Song
 from jiosaavn.utils import Cache
-from pathlib import Path
-from typing import Literal
-import tomllib
 
-from . import log, ic
 from .notify.Gotify import notify
+from .utils import ic, log
 
+ic.configureOutput(prefix=f"{Path(__file__).name} -> ")
 
-ic.configureOutput(prefix=f'{Path(__file__).name} -> ')
 
 class JiosaavnDownload:
-    
-    GOTIFY_CHANNEL: str|None = None
-    __gotify_key: str|None = None
-    
-    def __init__(self, cache_filepath: str|None = None, final_location: Path|None = None) -> None:
-        
+    GOTIFY_CHANNEL: str | None = None
+    __gotify_key: str | None = None
+
+    def __init__(
+        self, cache_filepath: str | None = None, final_location: Path | None = None
+    ) -> None:
         config = get_config()
         if cache_filepath is None:
-            cache_filepath = Path(config.get('paths',{}).get('db_folder', '.')) / 'database.pkl'
+            cache_filepath = (
+                Path(config.get("paths", {}).get("db_folder", ".")) / "database.pkl"
+            )
         if final_location is None:
-            final_location = Path(config.get('paths',{}).get('destination', '.'))
-        
-        log.info(f'Accessing database file at {cache_filepath}')
-        log.info(f'Final destination set to {final_location}')
-        channel_name = config.get('gotify', {}).get('app_name', '')
-        if channel_name != '':
+            final_location = Path(config.get("paths", {}).get("destination", "."))
+
+        log.info(f"Accessing database file at {cache_filepath}")
+        log.info(f"Final destination set to {final_location}")
+        channel_name = config.get("gotify", {}).get("app_name", "")
+        if channel_name != "":
             self.GOTIFY_CHANNEL = channel_name
-            self.GOTIFY_URL = config.get('gotify', {}).get('url', '')
-            if key:=config.get('gotify', {}).get('app_key', '') != '':
+            self.GOTIFY_URL = config.get("gotify", {}).get("url", "")
+            if key := config.get("gotify", {}).get("app_key", "") != "":
                 self.__gotify_key = key
-        
+
         self.cache_filepath: Path = Path(str(cache_filepath))
         self.cache = Cache(filepath=self.cache_filepath)
         self.set_downloader()
         self.final_location: Path = Path(str(final_location))
-    
+
     def set_downloader(self, downloader: SaavnMe = SaavnMe()):
         self.ApiProvider = downloader
-    
-    def song(self, url: str, skip_downloaded: bool = True, debug_only: bool=False):
-        self._download_song(song=self.ApiProvider.song(url=url), 
-                            skip_downloaded=skip_downloaded, debug_only=debug_only)
-    
+
+    def song(self, url: str, skip_downloaded: bool = True, debug_only: bool = False):
+        self._download_song(
+            song=self.ApiProvider.song(url=url),
+            skip_downloaded=skip_downloaded,
+            debug_only=debug_only,
+        )
+
     def _download_song(self, song: Song, skip_downloaded: bool, debug_only: bool):
         with song:
             if debug_only:
                 song.debug_mode = True
             if skip_downloaded and self.check_downloaded(song):
-                log.debug(f'Skipping {song.sanitized_name} from {song.sanitized_album}, Downloaded on {song.download_date}')
+                log.debug(
+                    f"Skipping {song.sanitized_name} from {song.sanitized_album}, Downloaded on {song.download_date}"
+                )
                 return
             _download_song(song=song, final_location=self.final_location)
-        
+
         if self.GOTIFY_CHANNEL:
             self.__gotify(song)
         if not debug_only:
             _cache_data = self.cache.data
             _cache_data.append(song)
-            self.cache.write(data = _cache_data)
+            self.cache.write(data=_cache_data)
         else:
-            log.debug('Cache will be updated here.')
-            
-    def __gotify(self, song:Song) -> None:
+            log.debug("Cache will be updated here.")
+
+    def __gotify(self, song: Song) -> None:
         assert self.GOTIFY_CHANNEL is not None
-        _title = f'[Jiosaavn]{song.sanitized_name}'
-        _msg = f'Album: {song.sanitized_album}\n\n ![]({song.image_url})'
-        notify(app=self.GOTIFY_CHANNEL,title=_title, message=_msg, 
-                priority=2,url=self.GOTIFY_URL, apptoken=self.__gotify_key)
-    
-    def playlist(self, id_link: str|int, skip_downloaded: bool = True, debug_only: bool=False):
-        if isinstance(id_link, str) and (id_link.startswith('http') or id_link.startswith('www')):
+        _title = f"[Jiosaavn]{song.sanitized_name}"
+        _msg = f"Album: {song.sanitized_album}\n\n ![]({song.image_url})"
+        notify(
+            app=self.GOTIFY_CHANNEL,
+            title=_title,
+            message=_msg,
+            priority=2,
+            url=self.GOTIFY_URL,
+            apptoken=self.__gotify_key,
+        )
+
+    def search_songs(
+        self, query: str, skip_downloaded: bool = True, debug_only: bool = False
+    ):
+        songs = self.ApiProvider.search_song(query=query)
+        for song in songs:
+            self._download_song(
+                song=song, skip_downloaded=skip_downloaded, debug_only=debug_only
+            )
+
+    def playlist(
+        self, id_link: str | int, skip_downloaded: bool = True, debug_only: bool = False
+    ):
+        if isinstance(id_link, str) and (
+            id_link.startswith("http") or id_link.startswith("www")
+        ):
             songs = self.ApiProvider.playlist(link=id_link)
         else:
             songs = self.ApiProvider.playlist(id=id_link)
         for song in songs:
-            self._download_song(song=song, skip_downloaded=skip_downloaded, debug_only=debug_only)
-        
+            self._download_song(
+                song=song, skip_downloaded=skip_downloaded, debug_only=debug_only
+            )
+
     def check_downloaded(self, song: Song) -> bool:
         """Checks if the specified song has previously been downloaded"""
         return song.id in [each.id for each in self.cache.data]
-    
-def _download_song(song: Song, final_location: Path|str) -> None:
+
+
+def _download_song(song: Song, final_location: Path | str) -> None:
     song.download()
     song.embed_metadata()
     song.move(finalpath=final_location)
-    
+
+
 def get_config() -> dict:
-    config_path: Path = Path(__file__).parent.parent.parent / 'config.toml'
+    config_path: Path = Path(__file__).parent.parent.parent / "config.toml"
     log.info(config_path)
     if config_path.exists():
-        with open(config_path, 'r') as f:
+        with open(config_path, "r") as f:
             return tomllib.loads(f.read())
-    else: 
+    else:
         config = {}
     return config
